c2b101d3220484e27d1d273dd2c2fedbe94d792d
LbnMahs <labanmahs2002@gmail.com>
2023-10-03 08:37:41 +0300

    Bounding box calculation function

--- signal-detection/app.py
+++ signal-detection/app.py
@@ -57,6 +57,79 @@ def select_mode(key, mode):
         mode = 1
     return number, mode
 
+
+# BOUNDING BOX CALCULATION FUNCTION
+def calc_bounding_rect(image, landmarks):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    landmark_array = np.empty((0, 2), int)
+
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+
+        landmark_point = [np.array((landmark_x, landmark_y))]
+
+        landmark_array = np.append(landmark_array, landmark_point, axis=0)
+
+    x, y, w, h = cv.boundingRect(landmark_array)
+
+    return [x, y, x + w, y + h]
+
+
+# LANDMARK LIST CALCULATION FUNCTION
+# def calc_landmark_list(image, landmarks):
+#     image_width, image_height = image.shape[1], image.shape[0]
+
+#     landmark_point = []
+
+#     for _, landmark in enumerate(landmarks.landmark):
+#         landmark_x = min(int(landmark.x * image_width), image_width - 1)
+#         landmark_y = min(int(landmark.y * image_height), image_height - 1)
+
+#         landmark_point.append([landmark_x, landmark_y])
+
+#     return landmark_point
+
+
+# LANDMARK LIST PREPROCESSING FUNCTION
+# def pre_process_landmark(landmark_list):
+#     temp_landmark_list = copy.deepcopy(landmark_list)
+
+#     # Converting to relative coordinates
+#     base_x, base_y = 0, 0
+#     for index, landmark_point in enumerate(temp_landmark_list):
+#         if index == 0:
+#             base_x, base_y = landmark_point[0], landmark_point[1]
+
+#         temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
+#         temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+
+#     # Converting to a one-dimensional list
+#     temp_landmark_list = list(
+#         itertools.chain.from_iterable(temp_landmark_list))
+
+#     # Normalization
+#     max_value = max(list(map(abs, temp_landmark_list)))
+
+#     def normalize_(n):
+#         return n / max_value
+
+#     temp_landmark_list = list(map(normalize_, temp_landmark_list))
+
+#     return temp_landmark_list
+
+
+# LOGGING LANDMARKS TO CSV FILE FUNCTION
+# def logging_csv(number, mode, landmark_list):
+#     if mode == 0:
+#         pass
+#     if mode == 1 and (0 <= number <= 9):
+#         csv_path = 'model_data/classifier_data/keypoints.csv'
+#         with open(csv_path, 'a', newline="") as f:
+#             writer = csv.writer(f)
+#             writer.writerow([number, *landmark_list])
+
 # ===== MAIN FUNCTION ===== #
 def main():
     # Parsing the arguments
